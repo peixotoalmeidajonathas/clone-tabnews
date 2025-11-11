803368dddb22d1a453e046717c8fc25c80e7f550
Jônathas de Almeida Alves Peixoto <peixotoalmeidajonathas@gmail.com>
2025-11-11 03:13:15 -0300

    Ajustes na tarefa especial, agora tudo ok

--- pages/mostraImagem.js
+++ pages/mostraImagem.js
@@ -1,7 +1,7 @@
 export default function MostraImagem(){
         return (
     <img 
-    src = "/mnt/d/clone-tabnews/pages/img/amores.jpg"
+    src = "/img/amores.jpg"
     alt = 'Casal feliz'
     style={{width: '150px', borderRadius:'12px'}}
     />
